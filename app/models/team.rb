0a8444e11ff74994968fe5016e35046ec12da5d6
kotentie <kotentie@gmail.com>
2015-10-21 18:07:10 -0500

    fix migrations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ActiveRecord::Base
+	has_many :players
+	belongs_to :hometeam, :class_name => 'Groupmatch'
+	belongs_to :awayteam, :class_name => 'Groupmatch'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015005255) do
+ActiveRecord::Schema.define(version: 20151021193536) do
+
+  create_table "games", force: :cascade do |t|
+    t.string   "map",        limit: 255
+    t.string   "score",      limit: 255
+    t.string   "videolink",  limit: 255
+    t.integer  "match_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "games", ["match_id"], name: "index_games_on_match_id", using: :btree
+
+  create_table "groupmatches", force: :cascade do |t|
+    t.integer  "match_id",    limit: 4
+    t.integer  "hometeam_id", limit: 4
+    t.integer  "awayteam_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  add_index "groupmatches", ["awayteam_id"], name: "index_groupmatches_on_awayteam_id", using: :btree
+  add_index "groupmatches", ["hometeam_id"], name: "index_groupmatches_on_hometeam_id", using: :btree
+  add_index "groupmatches", ["match_id"], name: "index_groupmatches_on_match_id", using: :btree
 
   create_table "matches", force: :cascade do |t|
-    t.string   "awayteam",      limit: 255
-    t.string   "hometeam",      limit: 255
     t.date     "matchdate"
+    t.integer  "matchorder",    limit: 4
     t.integer  "tournament_id", limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "matches", ["tournament_id"], name: "index_matches_on_tournament_id", using: :btree
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "team_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "players", ["team_id"], name: "index_players_on_team_id", using: :btree
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "sponsor",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "org",        limit: 255
+    t.date     "datestart"
+    t.date     "datefinish"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
