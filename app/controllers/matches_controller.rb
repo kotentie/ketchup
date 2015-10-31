2d24085714a23969a8b19167a2050a2bae5cd203
kotentie <kotentie@gmail.com>
2015-10-31 16:54:04 -0500

    add bootstrap and some styling

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,12 +3,13 @@ class MatchesController < ApplicationController
 def new
 end
 
-def create
+def index
+	@matches = Match.all 
 end
 
 def show
 	@match = Match.find(params[:id])
 	@games = Game.where(:match_id => params[:id])	
-end
+end	
 
 end
